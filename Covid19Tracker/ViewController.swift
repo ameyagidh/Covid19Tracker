972e8b5ffd382e2c69bbd7ad3d7475da76cdc75c
ameyagidh <ameyagb2@gmail.com>
2023-12-02 17:36:18 -0500

    Initial Commit

---
+++ Covid19Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid19Tracker
+//
+//  Created by Ameya Santosh Gidh on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Covid19TrackerUITests/Covid19TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Covid19TrackerUITestsLaunchTests.swift
+//  Covid19TrackerUITests
+//
+//  Created by Ameya Santosh Gidh on 12/2/23.
+//
+
+import XCTest
+
+final class Covid19TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
